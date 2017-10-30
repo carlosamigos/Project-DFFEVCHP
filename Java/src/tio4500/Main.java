3c96e4d604f7f781385419c4c63e754857e9be4b
Magnus Moan <magnusnm@ntnu.no>
2017-10-30 15:58:36 +0100

    Added java project

---
+++ Java/src/tio4500/Main.java
@@ -0,0 +1,12 @@
+package tio4500;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello, world!");
+	    RollingHorizon rolling = new RollingHorizon();
+	    rolling.run();
+    }
+}
