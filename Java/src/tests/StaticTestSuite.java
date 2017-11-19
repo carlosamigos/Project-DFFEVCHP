24030c960c110fbc163b2d2acb1dbb62c4af95ea
Magnus Moan <magnus.moan@gmail.com>
2017-11-19 11:41:07 +0100

    Debugging file path

--- Java/src/tests/StaticTestSuite.java
+++ Java/src/tests/StaticTestSuite.java
@@ -14,6 +14,11 @@ public class StaticTestSuite extends TestSuite {
 	}
 	
 	public void runTestSuite() {
+		
+		for(String test : testFileNames) {
+			System.out.println(test);
+		}
+		
 		System.out.println("\nStarting static test suite...");
 		System.out.println("Number of test files: " + testFileNames.size());
 		int runsLeft = this.solvers.size() * this.testFileNames.size();
