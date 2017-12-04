93538f8ab6e1c0b24910400f248cd9029d894c25
Magnus Moan <magnusnm@compute-4-57.local>
2017-12-04 18:15:59 +0100

    Static run

--- Java/src/tio4500/Main.java
+++ Java/src/tio4500/Main.java
@@ -10,10 +10,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+	/*
         DynamicTestSuite dynTest = new DynamicTestSuite(Constants.SolverType.MOSEL, Constants.NUMBER_OF_DAYS_TO_TEST);
         dynTest.runTestSuite();
 
-        /*ProblemInstance instance = new ProblemInstance(Constants.EXAMPLE_NUMBER);
+        ProblemInstance instance = new ProblemInstance(Constants.EXAMPLE_NUMBER);
         SimulationModel simulationModel = new SimulationModel(Constants.DAY_NUMBER,instance);
 
 
@@ -29,10 +30,10 @@ public class Main {
         DynamicProblem dynProb = new DynamicProblem(instance, simulationModel, Constants.SolverType.MOSEL);
         dynProb.solve();
 		
-
+	*/
         StaticTestSuite testSuite = new StaticTestSuite(Constants.SolverType.MOSEL);
         testSuite.runTestSuite();
-        */
+        
     }
 
 }
