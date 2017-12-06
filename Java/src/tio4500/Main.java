ecfd11ac77861efbc7c922bf7f949284404622eb
Magnus Moan <magnusnm@login-0-0.local>
2017-12-06 10:58:56 +0100

    Dynamic testing

--- Java/src/tio4500/Main.java
+++ Java/src/tio4500/Main.java
@@ -10,15 +10,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-	/*
         DynamicTestSuite dynTest = new DynamicTestSuite(Constants.SolverType.MOSEL, Constants.NUMBER_OF_DAYS_TO_TEST);
         dynTest.runTestSuite();
 
+	/*
         ProblemInstance instance = new ProblemInstance(Constants.EXAMPLE_NUMBER);
         SimulationModel simulationModel = new SimulationModel(Constants.DAY_NUMBER,instance);
 
 
-		/* Flyttes inn i SimulationModel-klassen?
         if(Constants.CREATE_NEW_SIMULATION_MODEL){
             simulationModel.createNewDaySimulationModel();
             simulationModel.saveDaySimulationModel();
@@ -30,10 +29,9 @@ public class Main {
         DynamicProblem dynProb = new DynamicProblem(instance, simulationModel, Constants.SolverType.MOSEL);
         dynProb.solve();
 		
-	*/
         StaticTestSuite testSuite = new StaticTestSuite(Constants.SolverType.MOSEL);
         testSuite.runTestSuite();
-        
+        */
     }
 
 }
