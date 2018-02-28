4e1bddfdd479487b71452920880461fc739fe5a6
Magnus Moan <magnus.moan@gmail.com>
2018-02-28 12:19:18 +0100

    Merge resolved

--- Java/src/code/solver/heuristics/tabusearch/TSIndividual.java
+++ Java/src/code/solver/heuristics/tabusearch/TSIndividual.java
@@ -121,7 +121,7 @@ public class TSIndividual extends Individual {
 	}
 
 	private double calculateDistanceCarmMove(Node previousNode, CarMove carMove){
-		return problemInstance.getTravelTimeBike(previousNode.getNodeId(), carMove.getFromNode().getNodeId()) + carMove.getTravelTime();
+		return problemInstance.getTravelTimeBike(previousNode, carMove.getFromNode()) + carMove.getTravelTime();
 	}
 
 	//Work to do
@@ -136,7 +136,7 @@ public class TSIndividual extends Individual {
 		for(Car car : carMovesCopy.keySet()){
 			if(carMovesCopy.get(car).size() > 0){
 				Node fromNode = carMovesCopy.get(car).get(0).getFromNode();
-				double distanceCandidate = problemInstance.getTravelTimeBike(node.getNodeId(), fromNode.getNodeId());
+				double distanceCandidate = problemInstance.getTravelTimeBike(node, fromNode);
 				if(distanceCandidate < distance){
 					distance = distanceCandidate;
 					double fitNess = Double.MAX_VALUE;
