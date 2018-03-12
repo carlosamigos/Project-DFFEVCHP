e814032695a36b5769a81445ce8792ce1ad6c178
simehe <simhellem@gmail.com>
2018-03-12 14:16:11 +0100

    Just testing

--- Java/src/constants/HeuristicsConstants.java
+++ Java/src/constants/HeuristicsConstants.java
@@ -8,15 +8,15 @@ public class HeuristicsConstants {
 	// Tabu Search
     public final static int TABU_NEIGHBORHOOD_SIZE = 30;
     public final static int TABU_SIZE = 10;
-    public final static int TABU_ITERATIONS = 10000;
+    public final static int TABU_ITERATIONS = 1000;
     public final static double TABU_CHARGING_UNIT_REWARD = 1;
     public final static double TABU_BREAK_CHARGING_CAPACITY = 100;
     public final static double TABU_SURPLUS_IDEAL_STATE_COST = 100;
     public final static double TABU_IDEAL_STATE_UNIT_REWARD = 40;
-    public final static double TABU_TRAVEL_COST = 0;
+    public final static double TABU_TRAVEL_COST = 0.001;
 
     //Punishes the size of operator list, relative to the travel time
-    public final static double TABU_SIZE_OF_OPERATOR_LIST = 0.01;
+    public final static double TABU_SIZE_OF_OPERATOR_LIST = 0.1;
     //------
 
     
@@ -31,7 +31,7 @@ public class HeuristicsConstants {
 
     
     
-    public final static double TABU_INTRA_MOVE_SIZE = 0.5*TABU_NEIGHBORHOOD_SIZE;
+    public final static double TABU_INTRA_MOVE_SIZE = 0.3*TABU_NEIGHBORHOOD_SIZE;
     public final static double TABU_INTER_MOVE_SIZE = 0.5*TABU_NEIGHBORHOOD_SIZE;
     public final static double TABU_INTER_2_SWAP_SIZE = 0.85*TABU_NEIGHBORHOOD_SIZE;
     public final static double TABU_EJECTION_REPLACE_SIZE = 0.05*TABU_NEIGHBORHOOD_SIZE;
