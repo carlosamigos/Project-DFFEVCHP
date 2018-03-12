967bbefef655ad75a89fac7dbf057a871821e4c5
Magnus Moan <magnus.moan@gmail.com>
2018-03-12 14:15:35 +0100

    Removed duplicates in neighborhood generation

--- Java/src/code/Main.java
+++ Java/src/code/Main.java
@@ -21,10 +21,10 @@ public class Main {
     	setConstants(args);
     	createTestingFolders();
 
-    	ProblemInstance problemInstance = new ProblemInstance(FileConstants.TEST_STATIC_FOLDER + "test_10nodes_2so_1c_4mov_4charging_0finishes_0MODE_a");
+    	ProblemInstance problemInstance = new ProblemInstance(FileConstants.TEST_STATIC_FOLDER + "test_40nodes_4so_3c_12mov_3charging_0finishes_0MODE_c");
     	TSSolver solver = new TSSolver(problemInstance);
     	solver.solve(problemInstance);
-    	System.out.println("Result: " + solver.getBest());
+    	System.out.println("Fitness: " + solver.getBest().getFitness() + "\n Result: " + solver.getBest());
 
     	if(!testing) {
 	    	TestSuite testSuite;
