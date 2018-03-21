880bec4d42466877dde180f04372a4eeb933800e
Magnus Moan <magnus.moan@gmail.com>
2018-03-21 10:49:15 +0100

    Added ALNS parameters

--- Java/src/constants/HeuristicsConstants.java
+++ Java/src/constants/HeuristicsConstants.java
@@ -6,8 +6,8 @@ public class HeuristicsConstants {
     public final static double MAX_THRESHOLD_CARMOVE_DISTANCE = 1.0;
 	 
 	// Tabu Search
-    public final static int TABU_NEIGHBORHOOD_SIZE = 100;
-    public final static int TABU_SIZE = 100;
+    public final static int TABU_NEIGHBORHOOD_SIZE = 200;
+    public final static int TABU_SIZE = 5;
     public final static int TABU_ITERATIONS = 20000;
     public final static int TABU_MAX_NON_IMPROVING_ITERATIONS = 25;
     public final static double TABU_CHARGING_UNIT_REWARD = 1;
@@ -15,6 +15,14 @@ public class HeuristicsConstants {
     public final static double TABU_SURPLUS_IDEAL_STATE_COST = 100;
     public final static double TABU_IDEAL_STATE_UNIT_REWARD = 30;
     public final static double TABU_TRAVEL_COST = 0;
+    
+    
+    public final static int ALNS_SEGMENT_LENGTH = 100;
+    public final static double ALNS_FOUND_NEW_SOLUTION = 1.0;
+    public final static double ALNS_FOUND_NEW_BEST_REWARD = 2.0;
+    public final static double ALNS_FOUND_NEW_GLOBAL_BEST_REWARD = 10.0;
+    public final static double ALNS_UPDATE_FACTOR = 0.5;
+    
 
     //Punishes the size of operator list, relative to the travel time
     public final static double TABU_SIZE_OF_OPERATOR_LIST = 1;
