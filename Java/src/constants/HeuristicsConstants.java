e6e20c2e15b2378f9e381bc725d1fb5246f45ee0
simehe <simhellem@gmail.com>
2018-04-03 11:18:16 +0200

    Bug fix regarding destroy

--- Java/src/constants/HeuristicsConstants.java
+++ Java/src/constants/HeuristicsConstants.java
@@ -8,7 +8,7 @@ public class HeuristicsConstants {
 	// Tabu Search
     public final static int TABU_NEIGHBORHOOD_SIZE = 200;
     public final static int TABU_SIZE = 2;
-    public final static int TABU_ITERATIONS = 2000;
+    public final static int TABU_ITERATIONS = 20000;
     public final static int TABU_MAX_NON_IMPROVING_ITERATIONS = 25000;
     public final static double TABU_CHARGING_UNIT_REWARD = 1;
     public final static double TABU_BREAK_CHARGING_CAPACITY = 100;
