65d433d134a56208b168400aa3b89aca62bc6c0b
Simen Hellem <simehe@compute-4-16.local>
2018-04-10 12:57:41 +0200

    Fixed dependencies

--- Testing/server.sh
+++ Testing/server.sh
@@ -184,10 +184,10 @@ fi
 echo "################"
 cd ../Java
 module load xpress/8.0.4
-module load java/9.0.1
+module load java/jdk-9.0.4
 ant clean
 ant build
 
 TEST_TYPE="${TYPE,,}"
 
-java -cp "./bin:/share/apps/xpress/8.0.4/lib/xprm.jar" tio4500.Main "$TEST_TYPE:$NAME" solver:$SOLVER
+java -cp "./bin:/share/apps/xpress/8.0.4/lib/xprm.jar" code.Main "$TEST_TYPE:$NAME" solver:$SOLVER
