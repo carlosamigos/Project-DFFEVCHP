88ad0d0173433dd39c160fb4dff5f6b3e1b8864b
Magnus Moan <magnusnm@compute-4-55.local>
2018-04-18 12:45:33 +0200

    Support for running only 1 test

--- Testing/copier.sh
+++ Testing/copier.sh
@@ -2,10 +2,12 @@
 
 FILES=(`ls -I copier.sh -I cleaner.sh`)
 
-for i in ${FILES[*]}; do
-	for j in $(seq 1 $1); do
-		NAME="${i::-4}"
-		cp $i ${NAME}_${j}.txt
+if [[ $1 -ge 1 ]]; then
+	for i in ${FILES[*]}; do
+		for j in $(seq 1 $1); do
+			NAME="${i::-4}"
+			cp $i ${NAME}_${j}.txt
+		done
+		rm $i
 	done
-	rm $i
-done
+fi
