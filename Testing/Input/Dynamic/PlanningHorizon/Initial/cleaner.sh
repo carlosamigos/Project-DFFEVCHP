399bd146637146a0c0bc963a42d6c59cfe6a4de7
magnus.moan@gmail.com <magnusnm@compute-4-50.local>
2018-05-10 13:46:16 +0200

    Time increments 5 -> 10

---
+++ Testing/Input/Dynamic/PlanningHorizon/Initial/cleaner.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+FILES=(`ls -I cleaner.sh -I copier.sh`)
+COUNT=0
+
+if [[ $1 -ge 2 ]]; then
+	echo "Removing file copies..."
+	for i in ${FILES[*]}; do
+		CHECK=$(($COUNT%$1))
+		if [[ $CHECK -ne 0 ]] ; then
+			rm $i
+		else
+			if [[ $1 -ge 9 ]] ; then
+				NAME="{$i::-7}"
+			else
+				NAME="${i::-6}"
+			fi
+			mv $i ${NAME}.txt
+		fi
+		let "COUNT++"
+	done	
+fi
+
