cf750b4e2be3d3afd453a2da9bdce9d8fb9de910
Magnus Moan <magnusnm@compute-4-56.local>
2018-05-11 23:54:37 +0200

    New static test case

--- Java/src/constants/HeuristicsConstants.java
+++ Java/src/constants/HeuristicsConstants.java
@@ -37,10 +37,10 @@ public class HeuristicsConstants {
 
     //LSO Weight
     public final static int ALNS_SEGMENT_LENGTH = 100;
-    public final static double ALNS_FOUND_NEW_SOLUTION = 2; //1.0
-    public final static double ALNS_FOUND_NEW_BEST_REWARD = 8.0; //2.0
+    public final static double ALNS_FOUND_NEW_SOLUTION = 1; //1.0
+    public final static double ALNS_FOUND_NEW_BEST_REWARD = 13.0; //2.0
     public final static double ALNS_FOUND_NEW_GLOBAL_BEST_REWARD = 23.0; //10.0
-    public final static double ALNS_UPDATE_FACTOR = 0.1; //0.5
+    public final static double ALNS_UPDATE_FACTOR = 0; //0.5
     public final static double ALNS_MINIMAL_WEIGHT_FACTOR = 0.05; //0.4
 
     //Initial solution
@@ -59,10 +59,10 @@ public class HeuristicsConstants {
     public final static double EARLIEST_DEPARTURE_WEIGHT = 0.05;
 
     //Large Neighborhood
-    public final static double ALNS_FOUND_NEW_SOLUTION_LNS = 8.0; //1.0
+    public final static double ALNS_FOUND_NEW_SOLUTION_LNS = 13.0; //1.0
     public final static double ALNS_FOUND_NEW_GLOBAL_BEST_REWARD_LNS = 23.0; //2.0
     public final static double ALNS_DESTROY_FACTOR = 0.4;
-    public final static double ALNS_UPDATE_FACTOR_LNS = 0.1; //0.5
+    public final static double ALNS_UPDATE_FACTOR_LNS = 0; //0.5
     public final static double ALNS_MINIMAL_WEIGHT_FACTOR_LNS = 0.05; //0.4
 
     //Enumeration of neighborhood
